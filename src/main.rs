04c811e66c74858d2dd7c624b083aa674c69162f
jesserwight <jessewright01@gmail.com>
2022-04-26 13:29:53 -0400

    tic tac

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use std::io::{stdin, stdout, Write};
+use std::io::{stdin, stdout, Stdout, Write};
 use termion::{
     clear, cursor,
     cursor::Goto,
     event::{Event, Key},
     input::TermRead,
-    raw::IntoRawMode,
+    raw::{IntoRawMode, RawTerminal},
     style,
 };
 
+#[derive(Copy, Clone)]
 enum Player {
     X,
     O,
@@ -33,13 +34,70 @@ impl Default for Player {
 
 struct Game {
     turn: Player,
+    cursor: usize,
     board: [Player; 9],
+    x: u16,
+    y: u16,
+}
+impl Game {
+    fn place_turn(&mut self) {
+        self.board[self.cursor] = self.turn;
+        self.toggle_player();
+    }
+    fn incr_x(&mut self) {
+        self.x = self.x + 1;
+    }
+    fn decr_x(&mut self) {
+        self.x = self.x - 1;
+    }
+    fn incr_y(&mut self) {
+        self.y = self.y + 1;
+    }
+    fn decr_y(&mut self) {
+        self.y = self.y - 1;
+    }
+
+    fn toggle_player(&mut self) {
+        match self.turn {
+            Player::X => self.turn = Player::O,
+            Player::O => self.turn = Player::X,
+            Player::Empty => {}
+        }
+    }
+    fn render(&self, stdout: &mut RawTerminal<Stdout>) -> Result<(), Box<dyn std::error::Error>> {
+        let buf = format!(
+            "      ╭─╮\r
+Turn: │{}│\r
+      ╰─╯\r
+            \r
+ {} │ {} │ {}\r
+───┼───┼───\r
+ {} │ {} │ {}\r
+───┼───┼───\r
+ {} │ {} │ {}\r
+",
+            self.turn,
+            self.board[0],
+            self.board[1],
+            self.board[2],
+            self.board[3],
+            self.board[4],
+            self.board[5],
+            self.board[6],
+            self.board[7],
+            self.board[8],
+        );
+        stdout.write(buf.as_bytes())?;
+        stdout.flush()?;
+        Ok(())
+    }
 }
 
 impl Default for Game {
     fn default() -> Self {
         Self {
             turn: Player::X,
+            cursor: 0,
             board: [
                 Default::default(),
                 Default::default(),
@@ -51,64 +109,42 @@ impl Default for Game {
                 Default::default(),
                 Default::default(),
             ],
+            x: 1,
+            y: 1,
         }
     }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut stdin = stdin().events();
-    let mut stdout = stdout().into_raw_mode()?; //this raw mode is doing something different
+    let mut stdout = stdout().into_raw_mode()?;
     let mut game = Game::default();
-    game.board[0] = Player::X;
     game.turn = Player::O;
 
     stdout.write(clear::All.to_string().as_bytes())?;
     stdout.flush()?;
     stdout.write(Goto(1, 1).to_string().as_bytes())?;
     stdout.flush()?;
-    // find the indexes for the positions and wrap around them in sequence as a matrix
-    let buf = format!(
-        "      ╭─╮\r
-Turn: │{}│\r
-      ╰─╯\r
-            \r
- {} │ {} │ {}\r
-───┼───┼───\r
- {} │ {} │ {}\r
-───┼───┼───\r
- {} │ {} │ {}\r
-",
-        game.turn,
-        game.board[0],
-        game.board[1],
-        game.board[2],
-        game.board[3],
-        game.board[4],
-        game.board[5],
-        game.board[6],
-        game.board[7],
-        game.board[8],
-    );
-    stdout.write(buf.as_bytes())?;
-    stdout.flush()?;
-    let (mut x, mut y) = (2, 1);
+
+    game.render(&mut stdout)?;
+
+    // A map needs to be made between the canvas and the board cursor (0-8)
+    // A true map is not needed if the is a mathmatical (constant) relation between them
+
     while let Some(Ok(ev)) = stdin.next() {
         match ev {
-            Event::Key(Key::Down) => {
-                y = y + 1;
-                write!(stdout, "{}", termion::cursor::Goto(x, y))?;
-            }
+            Event::Key(Key::Down) => {}
             Event::Key(Key::Up) => {
-                y = y - 1;
-                write!(stdout, "{}", termion::cursor::Goto(x, y))?;
+                write!(stdout, "{}", termion::cursor::Goto(*x, *y))?;
             }
             Event::Key(Key::Right) => {
-                x = x + 1;
-                write!(stdout, "{}", termion::cursor::Goto(x, y))?;
+                write!(stdout, "{}", termion::cursor::Goto(*x, *y))?;
             }
             Event::Key(Key::Left) => {
-                x = x - 1;
-                write!(stdout, "{}", termion::cursor::Goto(x, y))?;
+                write!(stdout, "{}", termion::cursor::Goto(*x, *y))?;
+            }
+            Event::Key(Key::Char('\n')) => {
+                game.place_turn();
             }
             Event::Key(Key::Ctrl('c')) => {
                 write!(stdout, "\r\nctrl-c")?;
@@ -130,4 +166,9 @@ Turn: │{}│\r
         cursor::Goto(1, 1)
     )?;
     return Ok(());
+}
+
+fn write_position(game: &Game, stdout: &RawTerminal<Stdout>) -> Result<(), std::io::Error> {
+    write!(stdout, "{}", termion::cursor::Goto(game.x, game.y))?;
+    Ok(())
 }
